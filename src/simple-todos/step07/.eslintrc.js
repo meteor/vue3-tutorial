72dcbd9fd3890f7ab7d063bd935d1707fbe48b5f
vitor <vi.souza.almeida@protonmail.com>
2023-08-05 18:53:14 -0300

    refactor(step07): remove unsecured packages

--- src/simple-todos/step07/.eslintrc.js
+++ src/simple-todos/step07/.eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 8,
+  },
   extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
   plugins: ['prettier'],
   rules: {
@@ -12,6 +15,7 @@ module.exports = {
     'vue/html-indent': ['error', 2],
     'vue/singleline-html-element-content-newline': 0,
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/multi-word-component-names': 0,
   },
   globals: {
     _: true,
---
+++ src/simple-todos/step07/imports/api/tasksMethods.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { TasksCollection } from '../api/TasksCollection';
+
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized.');
+    }
+
+    TasksCollection.insert({
+      text,
+      createdAt: new Date(),
+      userId: this.userId,
+    });
+  },
+
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized.');
+    }
+
+    TasksCollection.remove(taskId);
+  },
+
+  'tasks.setIsChecked'(taskId, checked) {
+    check(taskId, String);
+    check(checked, Boolean);
+
+    if (!this.userId) {
+      throw new Meteor.Error('Not authorized.');
+    }
+
+    TasksCollection.update(taskId, {
+      $set: {
+        checked,
+      },
+    });
+  },
+});
