9a4c4547ccd1dbde4d98633fd8f08d855ecb49f3
vitor <vi.souza.almeida@protonmail.com>
2023-08-05 17:56:57 -0300

    refactor(step06): remove unsecured packages

--- src/simple-todos/step04/imports/ui/components/TaskForm.vue
+++ src/simple-todos/step04/imports/ui/components/TaskForm.vue
@@ -13,9 +13,9 @@ const addTask = () => {
 <template>
   <form @submit.prevent="addTask">
     <input
+      v-model="newTask"
       class="border border-gray-300 rounded-md py-2 px-4 mr-2 text-gray-600 text-sm focus:outline-none focus:border-gray-400 focus:ring-0"
       type="text"
-      v-model="newTask"
       placeholder="Type to add new tasks"
     />
     <button
